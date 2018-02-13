30d481ae4df403e0e056461c7cc90c01dbce4ab2
paz schori <pazschori@gmail.com>
2018-02-13 21:38:45 +0200

    add output file for all OFFLINE computers.

--- testConnection.ps1
+++ testConnection.ps1
@@ -11,7 +11,29 @@ param(
 if ((test-path -path $filePath) -ne $true){
     write-host "$filePath is not valid please try again "
     exit
-}    
+}
+
+# check validation of outputFile for OFLINE computers if request by parameter
+Write-Verbose "checking if $outputFile is valid parameter (-outputFile) "
+if($outputFile -ne "" -and(Test-Path -Path $outputFile)-ne $true){
+    Write-Verbose "running new-item to create $outputFile"
+    Write-Host "run new-item func to create $outputFile for OFFLINE computers"
+    New-Item -Path $outputFile -ItemType File 
+    Write-Verbose "checking if $outputFile create successfully"
+    if ((Test-Path -Path $outputFile)-ne $true){
+        Write-Verbose "running test-path to validate new-item ($outputFile)create successfully"
+        Write-Host "failed to create $outputFile please try again"
+        exit
+    }
+}
+else{
+    Write-Verbose "$outputFile allready exist"
+    write-host "$outputFile allready exist" -ForegroundColor Yellow
+}
+# create func that write output to file
+function output ($computer) {
+    $computer >> $outputFile    
+}
 $computersList = Get-Content -Path $filePath
 foreach ($computer in $computersList){
     Write-Verbose "running test-connection func on $computer"  
@@ -19,6 +41,8 @@ foreach ($computer in $computersList){
         Write-Host "$computer is ONLINE" -ForegroundColor Green
     }
     else{
-        Write-Host "$computer IS OFLINE!!!" -ForegroundColor Red
+        Write-Host "$computer IS OFFLINE!!!" -ForegroundColor Red
+        Write-Verbose "adding OFFLINE computers to $outputFile"
+        output $computer
     }
 }
