820acfff274018640c63891f57cae007226d8789
paz schori <pazschori@gmail.com>
2018-02-13 20:39:51 +0200

    add foreach loop, test-connection func

--- testConnection.ps1
+++ testConnection.ps1
@@ -12,5 +12,13 @@ if ((test-path -path $filePath) -ne $true){
     write-host "$filePath is not valid please try again "
     exit
 }    
-
-
+$computersList = Get-Content -Path $filePath
+foreach ($computer in $computersList){
+    Write-Verbose "running test-connection func on $computer"  
+    if((Test-Connection -ComputerName $computer -Count 1 -Quiet) -eq $true){      
+        Write-Host "$computer is ONLINE" -ForegroundColor Green
+    }
+    else{
+        Write-Host "$computer IS OFLINE!!!" -ForegroundColor Red
+    }
+}
